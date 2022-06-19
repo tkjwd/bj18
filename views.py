1fbffd8990a5b7a2d24a5c5c8524123f4b91b2e2
tkjwd <524435617@qq.com>
2022-06-19 16:48:39 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
+
 
 def index(request):
     return HtpResponse('index')
 
+
+def login(request):
+    return redirect('/index')
